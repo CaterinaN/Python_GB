42506f602c220ff9199131c9b315c7de37963503
Alexsandr <katenok.konsta@mail.ru>
2022-11-24 14:09:25 +0300

    дз 2 задача ура

--- Sem/dz1sem_2zad.py
+++ Sem/dz1sem_2zad.py
@@ -1,39 +1,15 @@
 # 2- Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 # Нужно написать таблицу истинности.
 
-# def inputNumbers(x):
-#     xyz = ["X", "Y", "Z"]
-#     a = []
-#     for i in range(x):
-#         a.append(input(f"Введите значение {xyz[i]}: "))
-#     return a
-
-
-# def checkPredicate(x):
-#     first_statum = not (x[0] or x[1] or x[2])
-#     second_statum = not x[0] and not x[1] and not x[2]
-#     result = first_statum == second_statum
-#     return result
-
-
-# statement = inputNumbers(3)
-
-# if checkPredicate(statement) == True:
-#     print(f"Утверждение истинно")
-# else:
-#     print(f"Утверждение ложно")
-
-
-x = [0,0,0,0,1,1,1,1]
-y = [0,0,1,1,0,0,1,1]
-z = [0,1,0,1,0,1,0,1]
-for i in [x,y,z] :
-    print (x[],y[],z[])
-    first_statum = not (x[0] or y[0] or z[0])
-    print (first_statum)
-    second_statum = not x[0] and not y[0] and not z[0]
-    print (second_statum)
+x = int(input('Введите x : '))
+y = int(input('Введите y : '))
+z = int(input('Введите z : '))
+if x in range (0,2) and y in range (0,2) and z in range (0,2):
+    first_statum = not (x or y or z)
+    second_statum = not x and not y and not z
     if first_statum == second_statum:
-        print ('True')
+        print (x, y, z,'True')
     else:
-        print ('False')
+        print (x, y, z,'False')
+else:
+    print('введите числа в двоичной системе')
